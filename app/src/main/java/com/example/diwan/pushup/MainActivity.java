d151c1f93bdef390b497f10e7c7f3c180911df13
cps353 <cps353se@gmail.com>
2018-02-12 17:37:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/diwan/pushup/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.diwan.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void pushup(){
+
+        // written by di
+
+        int i = 0;
+    }
+
+    private void update(){
+        //
+    }
+}
